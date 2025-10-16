59083a622999ea3b4557c73575746abb4bc47677
Isha <isha.aggarwal@huulke.com>
2025-10-16 08:43:40 +0530

    updated

--- backend/src/app.js
+++ backend/src/app.js
@@ -32,6 +32,12 @@ app.use(bodyParser.json());
 
 app.use(cookieParser());
 
+app.get('/api/test-ip', (req, res) => {
+    console.log('Client IP:', req.ip);  // should NOT be 127.0.0.1 now
+    res.send(`Your IP is: ${req.ip}`);
+});
+
+
 app.use('/', authRouter);
 app.use('/', requestRouter);
 app.use('/', userRouter);
